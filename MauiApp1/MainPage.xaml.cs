e1ae771e77215fee776e2dc573ba2c3c733b8075
xwj-june <xwj.june@gmail.com>
2023-10-08 18:56:41 +1300

    simple countdown

--- MauiApp1/MainPage.xaml.cs
+++ MauiApp1/MainPage.xaml.cs
@@ -3,22 +3,33 @@
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        private TimeSpan countdownDuration = TimeSpan.FromMinutes(5);
+        private DateTime countdownEndTime;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private async void StartButton_Clicked(object sender, EventArgs e)
         {
-            count+=5;
+            // Calculate the end time of the countdown
+            countdownEndTime = DateTime.Now.Add(countdownDuration);
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+            // Disable the button during the countdown
+            startButton.IsEnabled = false;
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            // Update the countdown label every second
+            while (DateTime.Now < countdownEndTime)
+            {
+                TimeSpan remainingTime = countdownEndTime - DateTime.Now;
+                countdownLabel.Text = $"Countdown: {remainingTime:mm\\:ss}";
+                await Task.Delay(1000); // Delay for 1 second
+            }
+
+            // Re-enable the button when the countdown is finished
+            startButton.IsEnabled = true;
+            countdownLabel.Text = "Countdown: Done!";
         }
     }
 }
